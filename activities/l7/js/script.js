2d0d21a462109c314f354bc24faa5ec276ce45c6
Aarnav Lakhanpal <alakhanpal@timberlea.CS.Dal.Ca>
2024-09-27 19:42:13 -0300

    added index.html, scrpt.js and README.md for lecture7 activity

---
+++ activities/l7/js/script.js
@@ -0,0 +1,17 @@
+
+function randomNumberGenerator(){
+
+	console.log("The random number generated is: "+ Math.floor(Math.random() * 2000));
+}
+randomNumberGenerator();
+
+function areaOfCircle(){
+	const pi = 3.14;
+	const radius = 6;
+	const area = pi * radius * radius;
+
+	console.log("The area of the circle is: "+ area);
+}
+areaOfCircle();
+
+
